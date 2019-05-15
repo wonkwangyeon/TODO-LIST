8e0c8ed303d3a59c99bbf19d106a577052eb7054
PangYeon <cocoll2000@naver.com>
2019-05-15 15:41:52 +0900

    메인 네비게이션바 생성

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="head">
+      <hgroup>
+        <p class="title">
+          Todo List
+        </p>
+        <p class="subtitle">
+          Today : {{ new Date() | moment("YYYY 년 MM월 DD일 dddd") }}
+        </p>
+      </hgroup>
+    </div>
+
+    <!-- <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Acme|Source+Serif+Pro");
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+hgroup {
+  padding-top: 50px;
+  background-color: #00c6ed;
+}
+p.title {
+  margin-bottom: 0;
+}
+.title {
+  font-family: "Acme", sans-serif;
+  font-size: 50px;
+  color: #eaeaea;
+}
+p.subtitle {
+  padding-bottom: 20px;
+}
+.subtitle {
+  font-family: "Source Serif Pro", serif;
+  font-size: 20px;
+  color: #eaeaea;
 }
 </style>
---
+++ src/components/MainList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>    
+    
+  </div>
+</template>
+
+<script type="text/javascript">
+
+
+export default {
+  name: "input-todo",
+  data: function() {
+      return { todo: "" };
+  }
+};
+</script>
