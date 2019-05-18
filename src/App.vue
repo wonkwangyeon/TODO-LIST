0843ee6d7ded5f9feb70a097f4ed2a9690e54be1
PangYeon <cocoll2000@naver.com>
2019-05-18 22:45:47 +0900

    코드 정리에 누락된 파일 추가

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
   <div id="app">
-
     <MainNav />
-    <!-- <div class="router-size">-->
-    <!-- <router-link to="/" class="router-size">Home</router-link> -->
-    <!-- <b-button class="nav-btn" @click="$router.push('/')">Home</b-button> -->
-    <!-- <router-link to="/add"   class="router-size">Todo추가</router-link> -->
-    <!-- <b-button class="nav-btn" @click="$router.push('/add')">Add</b-button>
-    </div> -->
     <router-view />
-
   </div>
 </template>
 
 <script>
-//import HelloWorld from "./components/HelloWorld.vue";
-
 import MainNav from "./components/MainNav.vue";
 export default {
   name: "app",
@@ -25,17 +15,9 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Nanum Gothic", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
   color: #2c3e50;
-  /* margin-top: 60px; */
-}
-.router-size{
-   margin: 0 20px;
-}
-.nav-btn {
-  margin: 0 10px;
 }
 </style>
