97d6a0eb63f78b4b89b0dda0581abbb2881bfc23
PangYeon <cocoll2000@naver.com>
2019-05-19 18:18:43 +0900

    Heroku 배포를 위한 설정

--- backend/server.js
+++ backend/server.js
@@ -6,20 +6,24 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express();
 sequelize.sync();
-
-// Front-End에서의 접근을 위해Cross Origin Resource Sharing 허용 
+const PORT = process.env.PORT
+let corsURI = "http://localhost:9000"
+if (process.env.NODE_ENV === "production") {
+    corsURI = "http://2019-programmers-todo.wky.kr"
+}
+// Front-End에서의 접근을 위해Cross Origin Resource Sharing 허용
 app.use(cors({
-    origin: 'http://localhost:9000',
+    origin: corsURI,
     credentials: true
 }))
 
 app.use(bodyParser.json())
 app.use(morgan('common', { stream: logger.stream })); // request시 api정보를 확인하기 위한 morgan 설정
 
-var todoRouter = require('./routes/todo');
+let todoRouter = require('./routes/todo');
 app.use('/api/todo', todoRouter);
 
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
     logger.info('시작')
 });
